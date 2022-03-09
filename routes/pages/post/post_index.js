c89abb0e44d47d59ee5e13580189ebb6de6f56ba
Vitaliy Kybych <vitaliykyq@gmail.com>
2022-03-09 14:07:57 +0200

    Fix downloader music

--- routes/pages/post/post_index.js
+++ routes/pages/post/post_index.js
@@ -1,9 +1,6 @@
 const youtubeMp3Converter = require('youtube-mp3-converter')
-const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
 const ip = require("ip");
-
-const downloadPath = path.join("./music/");
 
 module.exports = async function(req, res) {
 
@@ -12,14 +9,14 @@ module.exports = async function(req, res) {
     "Channel: ".blue + req.body.channel + "\n" +
     "Link: ".blue + req.body.link + "\n");
 
-    const convertLinkToMp3 = youtubeMp3Converter(downloadPath);
+    const convertLinkToMp3 = youtubeMp3Converter('./');
 
     const pathToMp3 = await convertLinkToMp3(req.body.link);
 
-    const name = pathToMp3.replace("music//", "");
+    const name = pathToMp3.replace(".//", "");
     const token = req.body.token;
     const bot = new TelegramBot(token, { polling: false })
-    await bot.sendAudio(req.body.channel, downloadPath + name);
+    await bot.sendAudio(req.body.channel, "./" + name);
 
     res.send(true);
 };
